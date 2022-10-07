15f1d03b3c9bfb77189c48c71a466ef4bb5495d2
zx4545zx <zx4545zx>
2022-10-07 09:39:42 +0700

    hello 2

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  switch(req.method) {
+    case GET:
+      GetAll()
+      break;
+    case POST:
+      Post()
+      break;
+    default:
+      res.status(404).json({ name: 'error 404'})
+  }
+}
+
+const GetAll = () => {
+  res.status(200).json({name: "GET John Doe" })
+}
+
+const Post = () => {
+  res.status(200).json({name: "POST John Doe" })
 }
